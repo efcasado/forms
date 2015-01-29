3d88559b991575662cd28dff55305081a18884d6
Enrique Fernandez <efcasado@gmail.com>
2015-01-29 16:13:46 +0100

    Add support for modifying beam files

--- src/modules.erl
+++ src/modules.erl
@@ -43,6 +43,9 @@
     function/3
    ]).
 
+%% Type specifications
+-type mod() :: module() | forms:forms().
+
 
 %% ========================================================================
 %%  API
@@ -53,7 +56,10 @@
 %% Check if the given module has any '-export([])' attribute.
 %% @end
 %%-------------------------------------------------------------------------
--spec has_export_attr(forms:forms()) -> boolean().
+-spec has_export_attr(mod()) -> boolean().
+has_export_attr(Module)
+  when is_atom(Module) ->
+    has_export_attr(load_forms(Module));
 has_export_attr(Module) ->
     lists:any(fun({attribute,_,export,_}) ->  true;
                  (_) -> false end,
@@ -66,7 +72,12 @@ has_export_attr(Module) ->
 %% functions.
 %% @end
 %%-------------------------------------------------------------------------
--spec add_function(forms:form(), forms:forms(), boolean()) -> forms:forms().
+-spec add_function(forms:form(), mod(), boolean()) -> mod().
+add_function(Function, Mod, Exp)
+  when is_atom(Mod) ->
+    OldForms = load_forms(Mod),
+    NewForms = add_function(Function, OldForms, Exp),
+    beam(Mod, NewForms);
 add_function({function, _, Name, Arity, _} =  Fun, Mod, true = _Exp) ->
     Mod1 = '_add_function'(Fun, Mod),
     export_function(Name, Arity, Mod1);
@@ -82,7 +93,10 @@ add_function(Fun, Mod, false = _Exp) ->
 %% Add the function Name/Arity to the list of exported functions.
 %% @end
 %%-------------------------------------------------------------------------
--spec export_function(atom(), integer(), forms:forms()) -> forms:forms().
+-spec export_function(atom(), integer(), mod()) -> mod().
+export_function(Name, Arity, Module)
+  when is_atom(Module) ->
+    export_function(Name, Arity, load_forms(Module));
 export_function(Name, Arity, Module) ->
     [FileAttr, ModuleAttr| Forms] = Module,
     ExportAttr = {attribute, 0, export, [{Name, Arity}]},
@@ -93,7 +107,10 @@ export_function(Name, Arity, Module) ->
 %% Remove the function Name/Arity from the list of exported functions.
 %% @end
 %%-------------------------------------------------------------------------
--spec unexport_function(atom(), integer(), forms:forms()) -> forms:forms().
+-spec unexport_function(atom(), integer(), mod()) -> mod().
+unexport_function(Name, Arity, Module)
+  when is_atom(Module) ->
+    unexport_function(Name, Arity, load_forms(Module));
 unexport_function(Name, Arity, Module) ->
     forms:map(fun({attribute, L, export, ExpFuns}) ->
                       ExpFuns1 = lists:delete({Name, Arity}, ExpFuns),
@@ -111,10 +128,56 @@ unexport_function(Name, Arity, Module) ->
 %% '{function_not_found, {Name, Arity}}' exception.
 %% @end
 %%-------------------------------------------------------------------------
--spec function(atom(), integer(), forms:forms()) -> forms:form().
+-spec function(atom(), integer(), mod()) -> forms:form().
+function(Name, Arity, Mod)
+  when is_atom(Mod) ->
+    function(Name, Arity, load_forms(Mod));
 function(Name, Arity, []) ->
     throw({function_not_found, {Name, Arity}});
 function(Name, Arity, [{function, _, Name, Arity, _} = Fun| _Forms] = _Mod) ->
     Fun;
 function(Name, Arity, [_Other| Forms] = _Mod) ->
     function(Name, Arity, Forms).
+
+
+%% ========================================================================
+%% Local functions
+%% ========================================================================
+
+%%-------------------------------------------------------------------------
+%% @doc
+%% Load the forms of the specified module.
+%% @end
+%%-------------------------------------------------------------------------
+-spec load_forms(module()) -> forms:forms().
+load_forms(Module) ->
+    try
+        forms:read(Module)
+    catch
+        _:_ ->
+            throw({cannot_load_forms, Module})
+    end.
+
+beam(Module, Forms) ->
+    code:load_file(Module),
+    {file, File} = code:is_loaded(Module),
+    Dir = filename:dirname(File),
+    code:unstick_dir(Dir),
+    %% TODO: We may want to preserve some options from the original file.
+    Opts = [debug_info],
+    case compile:forms(Forms, Opts) of
+        {ok, Module, Binary} ->
+            case file:write_file(File, Binary) of
+                ok ->
+                    Module,
+                    code:purge(Module),
+                    code:load_file(Module);
+                {error, Reason} ->
+                    code:stick_dir(Dir),
+                    throw({beam_file_not_generated, Reason})
+            end;
+        Error ->
+            code:stick_dir(Dir),
+            throw(Error)
+    end,
+    code:stick_dir(Dir).
